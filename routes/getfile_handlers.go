bb4aa7ab7fd56c4c9683bde95a1e181e5a5f8799
Carlos <edumaderal1@hotmail.com>
2024-11-07 19:29:16 -0300

    removendo redimensionamento

--- routes/getfile_handlers.go
+++ routes/getfile_handlers.go
@@ -827,7 +827,7 @@ func GetfileHandler(c *gin.Context) {
 	ext := filepath.Ext(filePath)[1:] // Remove o ponto inicial da extensão
     mimeType := getMimeType(ext) // Captura o tipo MIME
 
-	content, err = resizeImage(content, 500, 0) // Define as dimensões desejadas
+	// content, err = resizeImage(content, 500, 0) // Define as dimensões desejadas
 	if err != nil { return }
     // Exibe o conteúdo encontrado no navegador
     c.Data(http.StatusOK, mimeType, content) 
